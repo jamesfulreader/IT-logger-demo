b12714c6798d5b0dab343b99c55fda92adab6afb
james <jamesfulreader@gmail.com>
2020-01-12 18:13:21 -0700

    created redux store

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddLogModal from './component/logs/AddLogModal';
 import EditLogModal from './component/logs/EditLogModal';
 import AddTechModal from './component/techs/AddTechModal';
 import TechListModal from './component/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -17,17 +19,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <Logs />
-        <TechListModal />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <Logs />
+          <TechListModal />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
